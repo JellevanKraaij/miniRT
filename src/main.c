b54f483aa8ea6fe27d5ad6a9e41771714d78e1e2
bde-meij <bde-meij@student.codam.nl>
2023-01-27 18:50:50 +0100

    feat: trying 2D tracings

--- src/main.c
+++ src/main.c
@@ -27,15 +27,110 @@ void	hook(void *param)
 
 int32_t	main(void)
 {
+	printf("hello\n");
+	int x = 0;
+	int y = 0;
+	int z = 0;
 	mlx_t	*mlx;
-
 	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
-	if (!mlx)
-		exit(EXIT_FAILURE);
-	g_img = mlx_new_image(mlx, 128, 128);
-	memset(g_img->pixels, 255, g_img->width * g_img->height * sizeof(int));
+	mlx_image_t	*g_img = mlx_new_image(mlx, WIDTH, HEIGHT);
 	mlx_image_to_window(mlx, g_img, 0, 0);
-	mlx_loop_hook(mlx, &hook, mlx);
+
+// draw camera or lightsource, with adjustable FOV
+	float FOV = 0.75;
+	while (y < HEIGHT)
+	{
+		while (x < WIDTH)
+		{
+			printf("%d\n", y);
+			if ((y > (-FOV*x + HEIGHT/2)) && (y < (FOV*x + HEIGHT/2)))
+				mlx_put_pixel(g_img, x, y, 0xbedefbFF);
+			x++;
+		}
+		x = 0;
+		y++;
+	}
+	x = 0;
+	y = 0;
+
+// // draw circle
+	int circle_size = 2500;
+	int circle_x_mid = WIDTH/2;
+	int circle_y_mid = HEIGHT/2;
+	while (y < HEIGHT)
+	{
+		while (x < WIDTH)
+		{
+			printf("%d\n", y);
+			if (((x - WIDTH/2) * (x - WIDTH/2) + (y - HEIGHT/2) * \
+				(y - HEIGHT/2)) <= circle_size)
+				mlx_put_pixel(g_img, x, y, 0x1b7e48FF);
+			x++;
+		}
+		x = 0;
+		y++;
+	}
+	x = 0;
+	y = 0;
+
+//show intersections (hitpoints)
+	int hit_point_object[100][2];
+	while (y < HEIGHT)
+	{
+		while (x < WIDTH)
+		{
+			if (((y > (-FOV*x + HEIGHT/2)) && (y < (FOV*x + HEIGHT/2))) && \
+			(((x - WIDTH/2) * (x - WIDTH/2) + (y - HEIGHT/2) * (y - HEIGHT/2)) \
+				<= circle_size))
+			{
+				hit_point_object[z][0] = x;
+				hit_point_object[z][1] = y;
+				z++;
+				mlx_put_pixel(g_img, x, y, 0xe40909FF);
+
+				// cast lines through hitpoints from centre of sphere 
+				if (y < HEIGHT / 2)
+				{
+					float diff_x = circle_x_mid - x;
+					float diff_y = circle_y_mid - y;
+					float exponent = diff_y / diff_x;
+					float super_c = ((exponent * x) - y ) * -1;
+					x = 0;
+					while (x < WIDTH / 2)
+					{
+						if ((((x * exponent)) + super_c < HEIGHT / 2) && \
+						((x * exponent) + super_c > 0))
+							mlx_put_pixel(g_img, x, (x * exponent) \
+							+ super_c,  0xe40909FF);
+						x++;
+					}
+				}
+
+				break ;
+			}
+			x++;
+		}
+	x = 0;
+	y++;
+	}
+	z--;
+
+// calculate and print degrees
+	float hit_x;
+	float hit_y;
+	int origin_camera[2];
+	origin_camera[0] = 0;
+	origin_camera[1] = HEIGHT/2;
+
+	printf("%d\n", z);
+	while (z > -1)
+	{
+		hit_x = hit_point_object[z][0] - origin_camera[0];
+		hit_y = hit_point_object[z][1] - origin_camera[1];
+		printf("%f\n", hit_y / hit_x);
+		z--;
+	}
+
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);

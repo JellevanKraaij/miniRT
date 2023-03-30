8405087400ba074178b589652128b5d4a09a2301
bde-meij <bde-meij@student.codam.nl>
2023-03-30 14:22:18 +0200

    add endcaps

--- src/render/hittables/cylinder.c
+++ src/render/hittables/cylinder.c
@@ -23,31 +23,59 @@ void cylinder_destroy(void *data)
 	free(data);
 }
 
-// double hit_cylinder(vec3 pixelVec, vec3 camera_origin, cylindrous cylin)
-// {
-// 	vec3 C = cylin.botcenter;
-// 	vec3 H = cylin.topcenter;
-// 	vec3 v = normalize_vec(vector_minus(pixelVec, camera_origin));
-// 	vec3 w = vector_minus(pixelVec, C);
-// 	vec3 h = normalize_vec(vector_minus(H, C));
+bool cylinder_hit_caps(const t_hittable *hittable, const t_ray *ray, t_hit_record *hit_record)
+{
+	t_vec3 center_bot = vec3_add_c(hittable->center, vec3_scalar(&hittable->orientation, ((t_cylinder *)hittable->data.data)->height / 2));
+	t_vec3 center_top = vec3_subtract_c(hittable->center, vec3_scalar(&hittable->orientation, ((t_cylinder *)hittable->data.data)->height / 2));
 
-// 	double a = (vector_product(v, v) - vector_product(v, h) * vector_product(v, h));
-// 	double b = 2 * (vector_product(v, w) - (vector_product(v, h) * vector_product(w, h)));
-// 	double c = vector_product(w, w) - pow(vector_product(w, h), 2) - (cylin.radius * cylin.radius);
+	double t_bot = vec3_dot_c(vec3_subtract(&center_bot, &ray->origin), hittable->orientation) / vec3_dot(&ray->direction, &hittable->orientation);
+	double t_top = vec3_dot_c(vec3_subtract(&center_top, &ray->origin), hittable->orientation) / vec3_dot(&ray->direction, &hittable->orientation);
 
-// 	double discriminal = b * b - 4 * a * c;
-// 	if (discriminal > 0)
-// 		return ((-b - sqrt(discriminal)) / 2*a);
-// 	return (0);
-// }
+	double t;
+	t_vec3 center;
+	if (t_bot < t_top)
+	{
+		center = center_bot;
+		t = t_bot;
+	}
+	else
+	{
+		center = center_top;
+		t = t_top;	
+	}
+
+	if (t < ray->min_distance || t > ray->max_distance)
+		return (false);
+
+	t_vec3 point = ray_at(ray, t);
+
+	t_vec3 CP = vec3_subtract(&center, &point);
+
+	if (vec3_lenght(&CP) > ((t_cylinder *)hittable->data.data)->radius)
+		return (false);
+
+	if (hit_record == NULL)
+		return (true);
+
+	hit_record->distance = t;
+	hit_record->point = point;
+	hit_record->object = hittable;
+
+	hit_record_set_normal(hit_record, ray, &hittable->orientation);
+	return (true);
+}
 
 bool cylinder_hit(const t_hittable *hittable, const t_ray *ray, t_hit_record *hit_record)
 {
-	t_vec3	oc = vec3_subtract(&ray->origin, &hittable->center);
-	double a = vec3_dot(&ray->direction, &ray->direction) - vec3_dot(&ray->direction, &hittable->orientation) * vec3_dot(&ray->direction, &hittable->orientation);
-	double b = 2 * (vec3_dot(&ray->direction, &oc) - (vec3_dot(&ray->direction, &hittable->orientation) * vec3_dot(&oc, &hittable->orientation)));
-	double c = vec3_dot(&oc, &oc) - (vec3_dot(&oc, &hittable->orientation) * vec3_dot(&oc, &hittable->orientation)) - (((t_cylinder *)hittable->data.data)->radius * ((t_cylinder *)hittable->data.data)->radius);
 	
+	if (cylinder_hit_caps(hittable, ray, hit_record))
+		return (true);
+
+	t_vec3	oc = vec3_subtract(&ray->origin, &hittable->center);
+	double 	a = vec3_dot(&ray->direction, &ray->direction) - vec3_dot(&ray->direction, &hittable->orientation) * vec3_dot(&ray->direction, &hittable->orientation);
+	double 	b = 2 * (vec3_dot(&ray->direction, &oc) - (vec3_dot(&ray->direction, &hittable->orientation) * vec3_dot(&oc, &hittable->orientation)));
+	double 	c = vec3_dot(&oc, &oc) - (vec3_dot(&oc, &hittable->orientation) * vec3_dot(&oc, &hittable->orientation)) - (((t_cylinder *)hittable->data.data)->radius * ((t_cylinder *)hittable->data.data)->radius);
+
 	double discriminant = (b * b) - (4 * a * c);
 	if (discriminant < 0)
 		return (false);
@@ -78,18 +106,6 @@ bool cylinder_hit(const t_hittable *hittable, const t_ray *ray, t_hit_record *hi
 	hit_record->point = point;
 	hit_record->object = hittable;
 	
-
-
-	// printf ("CP_len: %f CQ_len: %f radius: %f\n", CP_len, CQ_len, ((t_cylinder *)hittable->data.data)->radius);
-
-	t_vec3 CQ = vec3_scalar(&hittable->orientation, CQ_len);
-
-	t_vec3 Q = vec3_add(&hittable->center, &CQ);
-
-	t_vec3 normal = vec3_subtract(&hit_record->point, &Q);
-
-	normal = vec3_normalize(&normal);
-
-	hit_record_set_normal(hit_record, ray, &normal);
+	hit_record_set_normal(hit_record, ray, &VEC3_ZERO);
 	return (true);
 }
